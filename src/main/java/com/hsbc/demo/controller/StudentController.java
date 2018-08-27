229fae4285db82039bb67c0bd900f4f02915ad83
KG <kevingarnett1984@163.com>
2018-08-27 23:12:01 +0800

    Swagger2 with spring boot

--- src/main/java/com/hsbc/demo/controller/StudentController.java
+++ src/main/java/com/hsbc/demo/controller/StudentController.java
@@ -2,6 +2,9 @@ package com.hsbc.demo.controller;
 
 import com.hsbc.demo.bean.StudentBean;
 import com.hsbc.demo.service.StudentService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +17,11 @@ public class StudentController {
     @Autowired
     StudentService studentService;
 
+    @ApiOperation(value="Get student list", notes="Save student and refresh student list")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name="id", value="Student ID", required=true, dataType="String"),
+            @ApiImplicitParam(name="name", value="Student Name", required=false, dataType="String")
+    })
     @RequestMapping(value="/greeting", method=RequestMethod.GET)
     public List<StudentBean> greeting(@RequestParam(required = true) String id, @RequestParam(required = false, defaultValue = "")String name){
 
